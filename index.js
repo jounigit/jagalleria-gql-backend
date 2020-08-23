868af68bcf27f35db8511e0083c3a4fcf1f9791d
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2020-08-23 19:54:55 +0300

    init eslint

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const typeDefs = gql`
   type Query {
       hello: String
   }
-`;
+`
 
 const resolvers = {
-    Query: {
-      hello: () => "hello",
-    },
-  };
+  Query: {
+    hello: () => 'hello',
+  },
+}
 
-  logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
-  const options = { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
-      autoIndex: false
- }
-  mongoose
-    .connect(config.MONGODB_URI, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        autoIndex: false
-   })
-    .then( () => {
-      console.log('connected to database', config.MONGODB_URI)
-    })
-    .catch( err => {
-      console.log(err)
-    })
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: false
+  })
+  .then( () => {
+    console.log('connected to database', config.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
-const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers 
-});
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
